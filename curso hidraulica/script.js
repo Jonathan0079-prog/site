76850800e4567501807e0716e3f5ae6166a3b553
Jonathan0078 <92819780+Jonathan0078@users.noreply.github.com>
2025-07-01 16:47:31 -0300

    Update script.js

--- curso hidraulica/script.js
+++ curso hidraulica/script.js
@@ -1,14 +1,12 @@
-//
-// COLE AQUI O MESMO OBJETO `firebaseConfig` QUE VOCÊ USOU NO `login.js`
-//
+// Configuração do Firebase fornecida pelo usuário
 const firebaseConfig = {
-    apiKey: "AIza...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "...",
-    measurementId: "..."
+  apiKey: "AIzaSyB_yPeyN-_z4JZ4hny8x3neU3InyRl6OEg",
+  authDomain: "curso-hidraulica.firebaseapp.com",
+  projectId: "curso-hidraulica",
+  storageBucket: "curso-hidraulica.firebasestorage.app",
+  messagingSenderId: "119186516649",
+  appId: "1:119186516649:web:9c10d40022406b525757b8",
+  measurementId: "G-0DD784H7E0"
 };
 
 // Inicializa o Firebase
