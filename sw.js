a34d4c7850645193191ed16df0bdd61b144a55ba
Jonathan0078 <92819780+Jonathan0078@users.noreply.github.com>
2025-07-03 12:38:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,56 @@ const CACHE_NAME = 'manutencao-industrial-cache-v6'; // Atualize a versão a cad
 const APP_SHELL_URLS = [
   '/index.html',
   '/style.css',
-  '/images/icon-512x512.png'
-  'programas/index.html'
+  '/images/icon-512x512.png',
+  '/programas/index.html',
+  '/programas/style.css',
+  '/programas/script.js',
+
+  // Calculadora para Correias Transportadoras
+  '/programas/Calculadora para Correias Transportadoras/index.html',
+  '/programas/Calculadora para Correias Transportadoras/script.js',
+  '/programas/Calculadora para Correias Transportadoras/style.css',
+
+  // Conversor de Escalas Lineares
+  '/programas/Conversor de Escalas Lineares/index.html',
+  '/programas/Conversor de Escalas Lineares/script.js',
+  '/programas/Conversor de Escalas Lineares/style.css',
+
+  // LMS-SOFTWARE
+  '/programas/LMS-SOFTWARE/calculadora.html',
+  '/programas/LMS-SOFTWARE/index.html',
+  '/programas/LMS-SOFTWARE/mistura.html',
+  '/programas/LMS-SOFTWARE/plano.html',
+  '/programas/LMS-SOFTWARE/style.css',
+
+  // calculadora de montagem de rolamentos
+  '/programas/calculadora de montagem de rolamentos/index.html',
+  '/programas/calculadora de montagem de rolamentos/style.css',
+
+  // calculadora polias
+  '/programas/calculadora polias/index.html',
+  '/programas/calculadora polias/script.js',
+  '/programas/calculadora polias/style.css',
+
+  // calculadora quantidade e frequência de graxa
+  '/programas/calculadora quantidade e frequência de graxa/index.html',
+  '/programas/calculadora quantidade e frequência de graxa/script.js',
+  '/programas/calculadora quantidade e frequência de graxa/style.css',
+
+  // conversor de unidades
+  '/programas/conversor de unidades/index.html',
+  '/programas/conversor de unidades/script.js',
+  '/programas/conversor de unidades/style.css',
+
+  // relacao engrenagens
+  '/programas/relacao engrenagens/index.html',
+  '/programas/relacao engrenagens/script.js',
+  '/programas/relacao engrenagens/style.css',
+
+  // torque parafusos
+  '/programas/torque parafusos/index.html',
+  '/programas/torque parafusos/script.js',
+  '/programas/torque parafusos/style.css'
 ];
 
 // Instalação: cacheia arquivos essenciais
